b3cc897c04e8fd0c2b835ee03bddf478cd141bd9
Owais Malik <owaismalik@outlook.com>
2018-12-05 01:03:49 +0530

    commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send("IT works i think");
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
