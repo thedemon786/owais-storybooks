2ef667cc89f500cbbd477214bc91185ab15eb87f
Owais Malik <owaismalik@outlook.com>
2018-12-05 17:43:54 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
+//passport config
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,6 +14,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send("IT works i think");
 });
+
+//use routes
+app.use('/auth', auth);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
