ced4d2230cb4106f9af1954e0f95320b1f6dd766
Owais Malik <owaismalik@outlook.com>
2018-12-05 16:52:48 +0530

    one more commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://owais:owais123@ds125574.mlab.com:25574/storybook-dev',
+    googleClientID: '1088255602671-8dc48cacd26aq5g444l1lqfs1f50r81g.apps.googleusercontent.com',
+    googleClientSecret: 'hH_R12cgW6mTH4vlPVbXMkgf'
+}
