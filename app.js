cb744a44b8c9b640b5a61a364d49390b52d3dc7a
Owais Malik <owaismalik@outlook.com>
2018-12-12 02:41:40 +0530

    more routes and ui

--- app.js
+++ app.js
@@ -1,58 +1,84 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-//load user model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
-
-//load routes
+ 
+// Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
-//mongoose connect and load keys and map global promise
+// Load Keys
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+    truncate,
+    stripTags,
+    formatDate
+  } = require('./helpers/hbs');
+  
+// Map global promises
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
-.then(() => console.log('mongo connected'))
-.catch(err => console.log(err));
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
-//handlebars middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+ 
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
+    helpers: {
+      truncate: truncate,
+      stripTags: stripTags,
+      formatDate:formatDate
+    },
+    defaultLayout:'main'
+  }));
 app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
 }));
 
-//passport middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
-const port = process.env.PORT || 3000;
 
-//set global variables
-app.use((req, res, next) =>{
-res.locals.user = req.user || null;
-next();
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
-//use routes
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`)
 });
---
+++ routes/stories.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
+
+// Stories Index
+router.get('/', (req, res) => {
+  Story.find({status:'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
+});
+
+// Show Single Story
+router.get('/show/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .populate('user')
+  .then(story => {
+    res.render('stories/show', {
+      story: story
+    });
+  });
+});
+
+// Add Story Form
+router.get('/add', ensureAuthenticated, (req, res) => {
+  res.render('stories/add');
+});
+
+// Process Add Story
+router.post('/', (req, res) => {
+  let allowComments;
+
+  if(req.body.allowComments){
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments:allowComments,
+    user: req.user.id
+  }
+
+  // Create Story
+  new Story(newStory)
+    .save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+});
+
+module.exports = router;
